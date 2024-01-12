89d0545b3dcba3f44e19dd00d87de92050584e5d
Sebminecrafter <116523841+Sebminecrafter@users.noreply.github.com>
2024-01-12 08:08:59 -0500

    Add bots python files

---
+++ math-bot.py
@@ -0,0 +1,61 @@
+from discord.ext import commands
+import discord
+import random
+
+BOT_TOKEN = ""
+BOT_NAME = "math-bot"
+
+bot = commands.Bot(command_prefix="$", intents=discord.Intents.all())
+
+@bot.command()
+async def subtraction(ctx):
+	global correct
+	global current_question
+	num1 = random.randint(0, 500)
+	num2 = random.randint(0, num1)
+	correct = str(num1 - num2)
+	current_question = "subtract"
+	await ctx.send(f"What is {num1} - {num2} respond with $answer [your answer]")
+
+@bot.command()
+async def addition(ctx):
+	global correct
+	global current_question
+	num1 = random.randint(0, 500)
+	num2 = random.randint(0, 500)
+	correct = str(num1 + num2)
+	current_question = "add"
+	await ctx.send(f"What is {num1} + {num2} respond with $answer [your answer]")
+
+@bot.command()
+async def add(ctx, *nums):
+	result = 0
+	i = 0
+	for i in nums:
+		result = result + int(i)
+	await ctx.send(f"Result: {result}")
+
+@bot.command()
+async def subtract(ctx, num1, num2):
+	result = num1 - num2
+	await ctx.send(f"Result: {result}")
+
+@bot.command()
+async def answer(ctx, answer):
+	global correct
+	global current_question
+	if current_question == "":
+		await ctx.send("No current question!")
+	else:
+		if answer == correct:
+			if current_question == "add":
+				await ctx.send(f"THIS PERSON JUST DID ADDITION WOW THE ANSWER WAS {correct}!")
+			elif current_question == "subtract":
+				await ctx.send(f"THIS PERSON JUST DID SUBTRACTION WOW THE ANSWER WAS {correct}!")
+			correct = ""
+			current_question = ""
+		else:
+			await ctx.send("Incorrect answer!")
+
+
+bot.run(BOT_TOKEN)
---
+++ sebby_bot.py
@@ -0,0 +1,42 @@
+from discord.ext import commands
+import discord
+
+BOT_TOKEN = ""
+BOT_NAME = "sebby-bot"
+
+bot = commands.Bot(command_prefix="?", intents=discord.Intents.all(), help_command=None)
+
+@bot.event
+async def on_ready():
+    print("bot is ready")
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+@bot.command()
+async def ban(ctx, user, reason):
+    await ctx.send(f"/ban {user} {reason}")
+    embed=discord.Embed(title="Banned", description=f"Banned {user} for {reason}".format(ctx.message.author), color=0xff00f6)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def kick(ctx, user):
+    await ctx.send(f"/kick {user}")
+    embed=discord.Embed(title="Kicked", description=f"Kicked {user}".format(ctx.message.author), color=0xff00f6)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def help(ctx):
+    embed=discord.Embed(title="Help", description=f"""
+    Commands:
+    kick [USERNAME]          - kicks user
+    ban [USERNAME] [REASON]  - bans user
+    hello                    - says hello
+    list                     - shows this
+    """.format(ctx.message.author), color=0xff00f6)
+    await ctx.send(embed=embed)
+
+
+
+bot.run(BOT_TOKEN)
