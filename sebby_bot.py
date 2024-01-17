6d80f3e4c7a3775aa7645397108a59dab7ecdf2d
Sebminecrafter <116523841+Sebminecrafter@users.noreply.github.com>
2024-01-17 10:19:06 -0500

    Update sebby_bot.py

--- sebby_bot.py
+++ sebby_bot.py
@@ -1,10 +1,10 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
+import datetime
 
 BOT_TOKEN = ""
 BOT_NAME = "sebby-bot"
 
-client = discord.Client()
 bot = commands.Bot(command_prefix="?", intents=discord.Intents.all(), help_command=None)
 
 @bot.event
@@ -16,30 +16,41 @@ async def hello(ctx):
     await ctx.send("Hello!")
 
 @bot.command()
-async def ban(ctx, user, reason):
-    await ctx.send(f"/ban {user} {reason}")
-    embed=discord.Embed(title="Banned", description=f"Banned {user} for {reason}".format(ctx.message.author), color=0xff00f6)
+@commands.has_role("ALL POWERFUL")
+async def ban(ctx, member:discord.Member, *, reason):
+    if reason == None:
+        reason = "This user was banned by " + ctx.message.author.name
+    await member.ban(reason+reason)
+    embed=discord.Embed(title="Banned", description=f"Banned {member} for {reason}".format(ctx.message.author), color=0xff00f6)
     await ctx.send(embed=embed)
 
 @bot.command()
-async def kick(ctx, user):
-    await ctx.send(f"/kick {user}")
-    embed=discord.Embed(title="Kicked", description=f"Kicked {user}".format(ctx.message.author), color=0xff00f6)
+@commands.has_any_role("Admin", "ALL POWERFUL")
+async def kick(ctx, member:discord.Member, *, reason):
+    if reason == None:
+        reason = "This user was kicked by " + ctx.message.author.name
+    await member.kick(reason+reason)
+    embed=discord.Embed(title="Kicked", description=f"Kicked {member}".format(ctx.message.author), color=0xff00f6)
     await ctx.send(embed=embed)
+
+@bot.command()
+@commands.has_any_role("Admin", "ALL POWERFUL")
+async def mute(ctx, member:discord.Member, timelimit):
+    if "s" in timelimit:
+        gettime = timelimit.strip("s")
+        newtime = datetime.timedelta(seconds=int(gettime))
+    await member.edit(timed_out_until=discord.utils.utcnow() + newtime)
+    
 
 @bot.command()
 async def help(ctx):
     embed=discord.Embed(title="Help", description=f"""
     Commands:
-    kick [USERNAME]          - kicks user
+    kick [USERNAME] [REASON] - kicks user
     ban [USERNAME] [REASON]  - bans user
     hello                    - says hello
     help                     - shows this
     """.format(ctx.message.author), color=0xff00f6)
     await ctx.send(embed=embed)
 
-@client.event
-async def on_message(msg):
-
-client.run(BOT_TOKEN)
 bot.run(BOT_TOKEN)
